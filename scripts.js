5f299598074617f3eb0a0cce4d58677afeb2b964
Suigetsu-Isaac <isaacantonioalves@gmail.com>
2023-05-25 09:21:56 -0300

    consertando a remoção de dados

--- scripts.js
+++ scripts.js
@@ -8,24 +8,20 @@ const Modal = {
 };
 
 class Transaction {
-  transactions = []
-  incomeProps = 0
-  expenseProps = 0
+  transactions = [];
+  incomeProps = 0;
+  expenseProps = 0;
 
-  income() {
-    this.transactions.forEach((transaction) => {
-      if (transaction.amount > 0) this.incomeProps += transaction.amount;
-    });
-    return this.incomeProps;
+  calculateSums() {
+    this.incomeProps = this.transactions.reduce((total, transaction) => {
+      return transaction.amount > 0 ? total + transaction.amount : total;
+    }, 0);
+
+    this.expenseProps = this.transactions.reduce((total, transaction) => {
+      return transaction.amount < 0 ? total + transaction.amount : total;
+    }, 0);
   }
-  
-  expense() {   
-    this.transactions.forEach((transaction) => {
-      if (transaction.amount < 0) this.expenseProps += transaction.amount;
-    });
-    return this.expenseProps;
-  }
-  
+
   total() {
     return this.incomeProps + this.expenseProps;
   }
@@ -34,54 +30,51 @@ class Transaction {
 const trans = new Transaction();
 
 const DOM = {
-  Conteiner: document.querySelector("#data-table tbody"),
+  Container: document.querySelector("#data-table tbody"),
 
-  addTransaction(ind, transaction) {
-    const index = trans.transactions.length - 1; // Índice da transação na lista
+  addTransaction(index, transaction) {
     const tr = document.createElement("tr");
-    tr.setAttribute('data-index', index);
+    tr.setAttribute("data-index", index);
     tr.innerHTML = DOM.innerHTMLTransaction(transaction);
-    DOM.Conteiner.appendChild(tr);
+    DOM.Container.appendChild(tr);
 
-    const removeButton = tr.querySelector('img');
-    removeButton.addEventListener('click', (event) => {
-    const row = event.target.closest('tr');
-    const index = parseInt(row.getAttribute('data-index'));
+    const removeButton = tr.querySelector("img");
+    removeButton.addEventListener("click", (event) => {
+      const row = event.target.closest("tr");
+      const index = parseInt(row.getAttribute("data-index"));
 
-    // Chame o método para remover a transação e atualizar a tabela e os saldos
-    removeTransaction(index);
-  });
-
+      removeTransaction(index);
+    });
   },
 
   innerHTMLTransaction(transaction) {
     const CSSClass = transaction.amount > 0 ? "income" : "expense";
     const amount = Utils.formatCurrency(transaction.amount);
-    
+
     const html = `
     <tr>
       <td class="description">${transaction.description}</td>
       <td class=${CSSClass}>${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
-          <img src="./assets/minus.svg" id=${transaction.id} alt="Remover Transação" />
+          <img src="./assets/minus.svg" data-index="${transaction.id}" alt="Remover Transação" />
       </td>
     </tr>
     `;
     return html;
   },
 
-  updateBalance () {
+  updateBalance() {
     document.getElementById("IncomeDisplay").innerHTML = Utils.formatCurrency(
-       trans.income()
+      trans.incomeProps
     );
 
     document.getElementById("ExpenseDisplay").innerHTML = Utils.formatCurrency(
-       trans.expense()
+      trans.expenseProps
     );
 
     document.getElementById("TotalDisplay").innerHTML = Utils.formatCurrency(
-       trans.total()
+      trans.total()
     );
   },
 };
@@ -104,104 +97,76 @@ const url = "https://devfinance.onrender.com/";
 function removeTransaction(index) {
   const transactionId = trans.transactions[index].id;
 
-  // Remova a transação da lista usando o índice fornecido
   trans.transactions.splice(index, 1);
 
-  // Remova o registro da API usando o fetch
   fetch(`${url}${transactionId}`, {
-    method: 'DELETE',
+    method: "DELETE",
   })
     .then((response) => {
       if (response.ok) {
-        // Limpe a tabela e atualize-a novamente
-        DOM.Conteiner.innerHTML = '';
+        DOM.Container.innerHTML = "";
         trans.transactions.forEach((transaction, newIndex) => {
           DOM.addTransaction(newIndex, transaction);
         });
-
-        // Atualize os saldos
         DOM.updateBalance();
-      } else {
-        console.log('Ocorreu um erro ao excluir o registro.');
-      }
-    })
-    .catch((error) => {
-      console.log('Ocorreu um erro:', error);
-    });
-}
-
-
-function executarFetch() {
-
-
-  const addTransactionForm = document.getElementById('addTransactionForm');
-  addTransactionForm.addEventListener('submit', (event) => {
-  event.preventDefault(); // Evita o comportamento padrão de recarregar a página
-
-  // Obter os valores dos campos do formulário
-  const description = document.getElementById('description').value;
-  const amount = parseFloat(document.getElementById('amount').value) * 100;
-  const date = document.getElementById('date').value;
-
-  // Criar um objeto de transação com os valores
-  const newTransaction = {
-    description: description,
-    amount: amount,
-    date: date,
-  };
-
-  // Enviar a nova transação para a API usando o fetch
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(newTransaction),
+      } else {console.log("Ocorreu um erro ao excluir o registro.");
+    }
   })
-    .then((response) => response.json())
-    .then((data) => {
-      // Limpar o formulário após a adição bem-sucedida
-      addTransactionForm.reset();
+  .catch((error) => {
+    console.log("Ocorreu um erro:", error);
+  });
+}
+function executarFetch() {
+  const addTransactionForm = document.getElementById("addTransactionForm");
+  addTransactionForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // Evita o comportamento padrão de recarregar a página
 
-      // Adicionar a nova transação à lista e atualizar a tabela e os saldos
-      trans.transactions.push(data);
-      DOM.addTransaction(null, data);
-      DOM.updateBalance();
+    const description = document.getElementById("description").value;
+    const amount = parseFloat(document.getElementById("amount").value) * 100;
+    const date = document.getElementById("date").value;
+
+    const newTransaction = {
+      description: description,
+      amount: amount,
+      date: date,
+    };
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTransaction),
     })
-    .catch((error) => {
-      console.log('Ocorreu um erro:', error);
-    });
-});
+      .then((response) => response.json())
+      .then((data) => {
+        // Limpar o formulário após a adição bem-sucedida
+        addTransactionForm.reset();
 
+        trans.transactions.push(data);
+        DOM.addTransaction(trans.transactions.length - 1, data); // Usando o índice correto
+        DOM.updateBalance();
+      })
+      .catch((error) => {
+        console.log("Ocorreu um erro:", error);
+      });
+  });
 
-  // listagem de dados
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      // Adicione os dados à lista de transações
-      data.forEach((transaction) => {
-        trans.transactions.push(transaction);
+      trans.transactions = data; // Substituir a lista de transações
+
+      trans.transactions.forEach((transaction, index) => {
+        DOM.addTransaction(index, transaction); // Usando o índice correto
       });
 
-      // Atualize a tabela e os saldos
-      trans.transactions.forEach((transaction) => {
-        DOM.addTransaction(null, transaction);
-      });
-
+      trans.calculateSums(); // Recalcular as somas
       DOM.updateBalance();
     })
     .catch((error) => {
       console.log("Ocorreu um erro:", error);
     });
-} 
-
-
-
-
-
-
+}
 
 executarFetch();
-
-
-// Dentro da função executarFetch(), adicione o ouvinte de evento ao formulário
